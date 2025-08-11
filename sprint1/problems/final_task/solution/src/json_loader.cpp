57f59b1b4b35c9d84ccbf9cb460b12499d6841a9
Niggita0328 <churinnikita.04@gmail.com>
2025-08-11 22:54:04 +0300

    Add final_task

---
+++ sprint1/problems/final_task/solution/src/json_loader.cpp
@@ -0,0 +1,65 @@
+#include "json_loader.h"
+
+namespace json_loader {
+
+model::Game LoadGame(const std::filesystem::path& json_path) {
+    // Загрузить содержимое файла json_path, например, в виде строки
+    std::ifstream file_stream{json_path};
+    if (!file_stream) {
+        throw std::runtime_error("Failed to open file: " + json_path.string());
+    }
+
+    std::string content((std::istreambuf_iterator<char>(file_stream)), std::istreambuf_iterator<char>());
+    
+    // Распарсить строку как JSON, используя boost::json::parse    
+    auto root = boost::json::parse(content);
+    
+    // Загрузить модель игры из файла
+    model::Game game;
+    
+    for (const auto& map_json : root.as_object().at("maps").as_array()) {
+        const auto& map_obj = map_json.as_object();
+        
+        model::Map::Id id{std::string(map_obj.at("id").as_string())};
+        std::string name(map_obj.at("name").as_string());
+        model::Map map{id, name};
+
+        for (const auto& road_json : map_obj.at("roads").as_array()) {
+            const auto& road_obj = road_json.as_object();
+            model::Coord x0 = road_obj.at("x0").as_int64();
+            model::Coord y0 = road_obj.at("y0").as_int64();
+            if (road_obj.contains("x1")) {
+                model::Coord x1 = road_obj.at("x1").as_int64();
+                map.AddRoad(model::Road{model::Road::HORIZONTAL, {x0, y0}, x1});
+            } else {
+                model::Coord y1 = road_obj.at("y1").as_int64();
+                map.AddRoad(model::Road{model::Road::VERTICAL, {x0, y0}, y1});
+            }
+        }
+
+        for (const auto& building_json : map_obj.at("buildings").as_array()) {
+            const auto& building_obj = building_json.as_object();
+            model::Coord x = building_obj.at("x").as_int64();
+            model::Coord y = building_obj.at("y").as_int64();
+            model::Dimension w = building_obj.at("w").as_int64();
+            model::Dimension h = building_obj.at("h").as_int64();
+            map.AddBuilding(model::Building{model::Rectangle{{x, y}, {w, h}}});
+        }
+
+        for (const auto& office_json : map_obj.at("offices").as_array()) {
+            const auto& office_obj = office_json.as_object();
+            model::Office::Id office_id{std::string(office_obj.at("id").as_string())};
+            model::Coord x = office_obj.at("x").as_int64();
+            model::Coord y = office_obj.at("y").as_int64();
+            model::Dimension offset_x = office_obj.at("offsetX").as_int64();
+            model::Dimension offset_y = office_obj.at("offsetY").as_int64();
+            map.AddOffice(model::Office{office_id, {x, y}, {offset_x, offset_y}});
+        }
+        
+        game.AddMap(std::move(map));
+    }
+    
+    return game;
+}
+
+}  // namespace json_loader
