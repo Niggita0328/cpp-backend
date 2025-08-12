b9ca790fd9ef4c9842a34a3c4431fbd52406fa12
Niggita0328 <churinnikita.04@gmail.com>
2025-08-12 12:30:46 +0300

    Decompose json_loader

--- sprint1/problems/final_task/solution/src/json_loader.h
+++ sprint1/problems/final_task/solution/src/json_loader.h
@@ -7,6 +7,10 @@
 
 namespace json_loader {
 
+model::Road LoadRoad(const boost::json::object& road_obj);
+model::Building LoadBuilding(const boost::json::object& building_obj);
+model::Office LoadOffice(const boost::json::object& office_obj);
+model::Map LoadMap(const boost::json::value& map_json);
 model::Game LoadGame(const std::filesystem::path& json_path);
 
 }  // namespace json_loader
