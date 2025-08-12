732373cd8bcb368a5038c19e040f6a9b3bd2f6e7
Niggita0328 <churinnikita.04@gmail.com>
2025-08-12 12:05:26 +0300

    Update http_server.cpp

--- sprint1/problems/final_task/solution/src/http_server.h
+++ sprint1/problems/final_task/solution/src/http_server.h
@@ -21,9 +21,7 @@ using StringResponse = http::response<http::string_body>;
 using HttpRequest = StringRequest; // Используем StringRequest как основной тип запроса
 
 
-inline void ReportError(beast::error_code ec, std::string_view what) {
-    std::cerr << what << ": " << ec.message() << std::endl;
-}
+void ReportError(beast::error_code ec, std::string_view what);
 
 
 class SessionBase {
@@ -32,44 +30,20 @@ public:
     SessionBase(const SessionBase&) = delete;
     SessionBase& operator=(const SessionBase&) = delete;
 
-    void Run() {
-        // Вызываем метод Read, используя executor объекта stream_.
-        // Таким образом вся работа со stream_ будет выполняться, используя его executor
-        net::dispatch(stream_.get_executor(),
-                    beast::bind_front_handler(&SessionBase::Read, GetSharedThis()));
-    }
+    void Run();
 
 protected:
     beast::tcp_stream stream_;
     beast::flat_buffer buffer_;
     HttpRequest request_;
 
-    explicit SessionBase(tcp::socket&& socket)
-        : stream_(std::move(socket)) {
-    }
-
+    explicit SessionBase(tcp::socket&& socket);
     ~SessionBase() = default;
 
-    void Read() {
-        // Очищаем запрос от прежнего значения (метод Read может быть вызван несколько раз)
-        request_ = {};
-        stream_.expires_after(30s);
-        // Считываем request_ из stream_, используя buffer_ для хранения считанных данных
-        http::async_read(stream_, buffer_, request_,
-                         // По окончании операции будет вызван метод OnRead
-                         beast::bind_front_handler(&SessionBase::OnRead, GetSharedThis()));
-    }
-
-    void OnRead(beast::error_code ec, [[maybe_unused]] std::size_t bytes_read) {
-        if (ec == http::error::end_of_stream) {
-            // Нормальная ситуация - клиент закрыл соединение
-            return Close();
-        }
-        if (ec) {
-            return ReportError(ec, "read"sv);
-        }
-        HandleRequest(std::move(request_));
-    }
+    void Read();
+    void OnRead(beast::error_code ec, [[maybe_unused]] std::size_t bytes_read);
+    void OnWrite(bool close, beast::error_code ec, [[maybe_unused]] std::size_t bytes_written);
+    void Close();
 
     template <typename Body, typename Fields>
     void Write(http::response<Body, Fields>&& response) {
@@ -84,30 +58,11 @@ protected:
                           });
     }
 
-    void OnWrite(bool close, beast::error_code ec, [[maybe_unused]] std::size_t bytes_written) {
-        if (ec) {
-            return ReportError(ec, "write"sv);
-        }
-
-        if (close) {
-            // Семантика ответа требует закрыть соединение
-            return Close();
-        }
-
-        // Считываем следующий запрос
-        Read();
-    }
-
-    void Close() {
-        beast::error_code ec;
-        stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
-    }
-
     // Обработку запроса делегируем подклассу
     virtual void HandleRequest(HttpRequest&& request) = 0;
-
     virtual std::shared_ptr<SessionBase> GetSharedThis() = 0;
 };
+
 
 template <typename RequestHandler>
 class Session : public SessionBase, public std::enable_shared_from_this<Session<RequestHandler>> {
@@ -117,6 +72,7 @@ public:
         : SessionBase(std::move(socket))
         , request_handler_(std::forward<Handler>(request_handler)) {
     }
+    
 private:
     std::shared_ptr<SessionBase> GetSharedThis() override {
         return this->shared_from_this();
@@ -195,6 +151,7 @@ private:
     tcp::acceptor acceptor_;
     RequestHandler request_handler_;
 };
+
 
 template <typename RequestHandler>
 void ServeHttp(net::io_context& ioc, const tcp::endpoint& endpoint, RequestHandler&& handler) {
