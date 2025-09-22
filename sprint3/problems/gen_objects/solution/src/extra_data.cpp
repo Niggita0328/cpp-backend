ff8a84978b6be020b49fc54f6e159988e4200f32
Niggita0328 <churinnikita.04@gmail.com>
2025-09-22 04:35:31 +0300

    Add gen_objects

---
+++ sprint3/problems/gen_objects/solution/src/extra_data.cpp
@@ -0,0 +1,35 @@
+﻿#include "extra_data.h"
+
+namespace extra_data {
+
+void Repository::Clear() noexcept {
+    maps_.clear();
+}
+
+void Repository::SetMapData(MapData data) {
+    std::string key = data.id;
+    maps_.insert_or_assign(std::move(key), std::move(data));
+}
+
+const MapData* Repository::FindMapData(const std::string& id) const noexcept {
+    if (auto it = maps_.find(id); it != maps_.end()) {
+        return &it->second;
+    }
+    return nullptr;
+}
+
+const boost::json::array* Repository::FindLootTypes(const std::string& id) const noexcept {
+    if (auto* map_data = FindMapData(id)) {
+        return &map_data->loot_types;
+    }
+    return nullptr;
+}
+
+std::size_t Repository::GetLootTypesCount(const std::string& id) const noexcept {
+    if (auto* loot = FindLootTypes(id)) {
+        return loot->size();
+    }
+    return 0;
+}
+
+}  // namespace extra_data
