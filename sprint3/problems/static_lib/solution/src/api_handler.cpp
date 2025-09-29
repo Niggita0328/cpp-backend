b6105e3662cbe1935f548baa8f3ba726c6c48a88
Niggita0328 <churinnikita.04@gmail.com>
2025-09-29 20:09:40 +0300

    Add gather-tests

---
+++ sprint3/problems/static_lib/solution/src/api_handler.cpp
@@ -0,0 +1,26 @@
+#include "api_handler.h"
+
+namespace http_handler {
+
+ApiHandler::ApiHandler(app::Application& app, bool manual_tick)
+    : app_{app}, manual_tick_{manual_tick} {
+}
+
+StringResponse ApiHandler::MakeStringResponse(http::status status, std::string_view body, unsigned version, bool keep_alive, http::verb method, std::string_view content_type, std::optional<std::pair<http::field, std::string_view>> extra_header) {
+    StringResponse res{status, version};
+    res.set(http::field::content_type, std::string(content_type));
+    res.set(http::field::cache_control, "no-cache");
+    if(extra_header) {
+        res.set(extra_header->first, std::string(extra_header->second));
+    }
+    res.content_length(body.size());
+    res.keep_alive(keep_alive);
+
+    if (method != http::verb::head) {
+        res.body() = std::string(body);
+    }
+    
+    return res;
+}
+
+}  // namespace http_handler
---
+++ sprint3/problems/static_lib/solution/src/json_loader.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <boost/json.hpp>
+
+#include "extra_data.h"
+#include "model.h"
+
+namespace keys {
+
+    constexpr auto MAPS = "maps";
+    constexpr auto ID = "id";
+    constexpr auto NAME = "name";
+    constexpr auto DOG_SPEED = "dogSpeed";
+    constexpr auto DEFAULT_DOG_SPEED = "defaultDogSpeed";
+
+    constexpr auto ROADS = "roads";
+    constexpr auto X0 = "x0";
+    constexpr auto Y0 = "y0";
+    constexpr auto X1 = "x1";
+    constexpr auto Y1 = "y1";
+
+    constexpr auto BUILDINGS = "buildings";
+    constexpr auto X = "x";
+    constexpr auto Y = "y";
+    constexpr auto WIDTH = "w";
+    constexpr auto HEIGHT = "h";
+
+    constexpr auto OFFICES = "offices";
+    constexpr auto OFFSET_X = "offsetX";
+    constexpr auto OFFSET_Y = "offsetY";
+
+    constexpr auto LOOT_GENERATOR_CONFIG = "lootGeneratorConfig";
+    constexpr auto PERIOD = "period";
+    constexpr auto PROBABILITY = "probability";
+    constexpr auto LOOT_TYPES = "lootTypes";
+    constexpr auto FILE = "file";
+    constexpr auto TYPE = "type";
+    constexpr auto ROTATION = "rotation";
+    constexpr auto COLOR = "color";
+    constexpr auto SCALE = "scale";
+
+} // namespace keys
+
+namespace json_loader {
+
+struct LoadedGameData {
+    model::Game game;
+    extra_data::Repository extra_data;
+};
+
+LoadedGameData LoadGame(const std::filesystem::path& json_path);
+
+}  // namespace json_loader
